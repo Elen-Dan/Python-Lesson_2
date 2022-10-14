039cc80f1ccca9dbd7d20153be50a51680796a96
El_Dan <lilit-li@ya.ru>
2022-10-14 19:19:47 +0400

    Решение задачи 1

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,20 @@
-import random
+'''
+1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+Пример:
+ 6782 -> 23
+ 0,56 -> 11
+'''
 
-i = int(input('Введите диапазон:\n'))
-n = list(range(i))
-print(n)
-# перемешаем список:
-random.shuffle(n)
-print('Перемешанный список:\n')
-print(n)
+from ast import Break
+
+
+x = str(input('Введите вещественное число: '))
+sum = 0
+if int(x) < 0:
+    print('Число меньше нуля')
+    Break
+else:    
+    for i in range(len(x)):
+        if x[i] != '.' and x[i] != ',':
+            sum += int(x[i])
+    print(sum)
